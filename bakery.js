8943c99baf962afd988ee7960d22976bdb93be94
geeta <ggmakadi1997@gmail.com>
2022-06-17 10:41:14 -0400

    gggggggggggggggggggggggggggggproduct

---
+++ bakery.js
@@ -0,0 +1,293 @@
+foodsData = [
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/a1fd7d55-1a41-4c6f-9927-cbf4f458f0ad_425x425.jpg",
+        name: "KEVENTER DELIGHTS COFFEE SHK 180ML ..",
+        price: "MRP₹519.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/93052e3b-98de-4d14-a701-ce9f584ca562_425x425.jpg",
+        name: "KEVENTER DELIGHTS CHOCO SHK 180ML T..",
+        price: "MRP₹369.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/342c136e-8e1a-4141-9ae7-feefb41257d9_425x425.jpg",
+        name: "KEVENTER DELIGHTS STRAWBRY SHK 180M..",
+        price: "MRP₹249.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/72df9e3c-85ac-4b48-93ac-107f9c058fb5_425x425.jpg",
+        name: "NESTLE A+GREEK YOGHURT STRAWBERRY 1..",
+        price: "MRP₹159.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/28662c49-d5cd-43c4-9209-21cae7949d2c_425x425.jpg",
+        name: "Mamie Yova French Natural Yoghurt 1..",
+        price: "MRP₹199.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/1371231_1_425x425.jpg",
+        name: "EPIGAMIA GREEK YOGURT NSA MXD BRRY ..",
+        price: "MRP₹159.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/1371230_1_425x425.jpg",
+        name: "EPIGAMIA GREEK YOGURT NSA STRWBRRY ..",
+        price: "MRP₹299.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5e50d332-c96f-4aa0-bed5-2cd48138e3fe_425x425.jpg",
+        name: "MAMIE YOVA VANILLA DRINK YOGHURT 12..",
+        price: "MRP₹229.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/f9ebebd6-4eec-47a1-a9e4-964a15b0de73_425x425.jpg",
+        name: "Epigamia Greek Yogurt - Strawberry ..",
+        price: "MRP₹59.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/e8042587-6211-4304-b8e3-c681a5ea90cb_425x425.jpg",
+        name: "NATURE VLY CRU GRA BAR RST ALMD 252..",
+        price: "MRP₹350",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/2c438352-3b1d-44ee-ad8f-2f977617377e_425x425.jpg",
+        name: "MAMIE YOVA BLUEBERRY STIRRED YOGHUR..",
+        price: "MRP₹159.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/c8bff082-846f-45e0-b2fc-52ff312d35b5_425x425.jpg",
+        name: "MAMIE YOVA MANGO STIRRED YOGHURT 90..",
+        price: "MRP₹79.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/4530e96d-084a-4390-b859-9642214ce72c_425x425.jpg",
+        name: "NESTLE A+GREEK YOGHURT MANGO 100G",
+        price: "MRP₹99.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/681eacbc-7ee8-4578-8645-3903aaf5e1a5_425x425.jpg",
+        name: "MAMIE YOVA BANANA DRINK YOGHURT 125..",
+        price: "MRP₹49.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/bd76ca3b-660c-4602-b7bb-ec9c8353c7cb_425x425.jpg",
+        name: "MAMIE YOVA LYCHEE STIRRED YOGHURT 9..",
+        price: "MRP₹59.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/9dd15c15-f92d-4f94-888b-886eddb9edfa_425x425.jpg",
+        name: "MAMIE YOVA PINACOLADA DRINK YOGHURT..",
+        price: "MRP₹359.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/0e6ef359-783c-42c5-a138-81194b8b99b1_425x425.jpg",
+        name: "MAMIE YOVA STRAWBERY STIRRRED YOGHU..",
+        price: "MRP₹159.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/8c24c5d7-8be8-4831-b101-d4adc2c57c4a_425x425.jpg",
+        name: "MAMIE YOVA MANGO DRINK YOGHURT 125M..",
+        price: "MRP₹259.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/0cbe7c04-2ed6-4601-879d-a22832b60baa_425x425.jpg",
+        name: "NATURE VALLEY OATS & HONEY 252G",
+        price: "MRP₹99.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/ec73f596-2243-4ad3-823d-16ddec49035f_425x425.jpg",
+        name: "Epigamia Greek Yogurt - Alphonso Ma..",
+        price: "MRP₹109.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/85934e90-421c-4542-9f26-9a1ed04fdf2a_425x425.jpg",
+        name: "Himalayan Natives Multifloral Raw H..",
+        price: "MRP₹59.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/41237bbd-636e-4d77-b0e7-bbbdfd21930f_425x425.jpg",
+        name: "TBD CHOCOLATE EXPLOSION COOKIES 200..",
+        price: "MRP₹159.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/Images/no-images425x425.jpg",
+        name: "L EXCLUSIF COFFEE CRUMBLE CAKE 250G",
+        price: "MRP₹119.0",
+       
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/1326677_1_425x425.jpg",
+        name: "KELLOGG's CRUNCHY GRANOLA 460G",
+        price: "MRP₹119.0",
+        
+    },
+    {
+       image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/74bf1323-3278-4e4c-89b4-a4dec49d663b_425x425.jpg",
+        name: "THF PRO NUTRI LOAF 250G",
+        price: "MRP₹219.0",
+        
+    },
+    {
+        image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/62dbf407-7434-457e-af60-29f772317460_425x425.jpg",
+         name: "So Good Almond Milk Chocolate Flavo..",
+         price: "MRP₹319.0",
+        
+     },
+     {
+        image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/1259519_1_425x425.jpg",
+         name: "KELLOGGS MUESLI NO ADDED SUGAR 500G",
+         price: "MRP₹109.0",
+        
+     },
+     {
+        image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5a6e1306-2151-4725-b046-10b8187f5d4f_425x425.jpg",
+         name: "SPRINNG PLAIN BREAD GLUTEN FREE 200..",
+         price: "MRP₹119.0",
+        
+     },
+     {
+        image_url: "https://d1z88p83zuviay.cloudfront.net/Images/no-images425x425.jpg",
+         name: "L EXCLUSIF BANANA WANUT CAKE 250G",
+         price: "MRP₹19.0",
+         
+     },
+     {
+        image_url: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/1328857_1_425x425.jpg",
+         name: "BOBS RED MILL THICK ROLLED OATS",
+         price: "MRP₹119.0",
+        
+     }
+ ]
+  
+
+  var basketLSData = JSON.parse(localStorage.getItem("basket")) || [];
+  document.querySelector("#filter").addEventListener("change", handle_sort);
+  
+  function handle_sort(){
+    var sort_type = document.querySelector("#filter").value;
+    
+    if(sort_type=="none"){
+      Display(foodsData);
+    }
+    else if(sort_type == "asc" ){
+      foodsData.sort(function (a, b){
+        return a.price - b.price;
+      });
+      // console.log(dressData);
+      Display(foodsData);
+    }
+    else if(sort_type == "dec"){
+      foodsData.sort(function (a,b){
+        return b.price - a.price;
+      });
+      Display(foodsData);
+    }
+    else{
+      var filtered = foodsData.filter(function (elem){
+        return elem.category == sort_type;
+      })
+      Display(filtered);
+    }
+  }
+      // console.log(dressData)
+  
+      Display(foodsData);
+
+  function Display(foodsData){
+  
+    document.querySelector("#beauty_product_container").innerHTML ="";
+    foodsData.forEach(function(elm){
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "avtar");
+    var img = document.createElement("img");
+    img.style.width = "250px";
+
+    img.setAttribute("src", elm.image_url);
+    var Name = document.createElement("h3");
+    Name.style.fontSize = "15px";
+    Name.style.color = "gray";
+    Name.style.marginLeft ="12px"
+    Name.innerText = elm.name;
+    var div2 = document.createElement("div")
+        div2.id = "box_div"
+
+    var dollar = document.createElement("h4");
+    
+    dollar.style.marginLeft = "0px"
+    dollar.style.display = "inline"
+
+
+    var Price = document.createElement("button");
+    Price.innerText = elm.price;
+     Price.style.marginLeft = "10px"
+    // Price.style.marginTop = "15px"
+    Price.setAttribute("id","rupya")
+    Price.style.color="gray"
+   
+
+    var buton = document.createElement("button");
+    buton.innerText = "Add";
+    buton.style.display = "block"
+    // button.style.marginBottom="0px";
+     buton.style.marginLeft="80px";
+     buton.style.marginRight="10px";
+    buton.setAttribute("id", "add_to_basket");
+    buton.addEventListener("click", Addtobasket);
+    function Addtobasket(){
+      var addedProduct = {
+        image: elm.image_url,
+        name: elm.name,
+        price: elm.price
+      }
+      // console.log(elm.image_url);
+      basketLSData.push(addedProduct);
+      localStorage.setItem("basket", JSON.stringify(basketLSData));
+      alert("Product added Successfully");
+    }
+    div2.append(Price, buton)
+    div.append(img, Name, dollar,div2);
+
+    var add = {
+      image: elm.image_url,
+      name: elm.name,
+      price: elm.price
+    }
+    div.onclick = () => {
+      playvideo(add);
+    };
+    document.querySelector("#beauty_product_container").append(div);
+   
+
+
+
+  });
+}
+ Display(foodsData);
+ let playvideo = (add) => {
+   console.log(add);
+      localStorage.setItem("individual",JSON.stringify(add));
+      window.location.href="addpro.html";
+ };
